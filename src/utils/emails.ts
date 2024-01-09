117e71c807cbfcf6e46608279a72bd9306b6b728
Momezz <julgomez14gmail.com>
2024-01-08 20:53:58 -0500

    fix: create a function and route to send emails

---
+++ src/utils/emails.ts
@@ -0,0 +1,26 @@
+import nodemailer from 'nodemailer';
+
+const gmailUser = process.env.GMAIL_USER;
+const gmailPassword = process.env.GMAIL_PASSWORD;
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: gmailUser,
+    pass: gmailPassword,
+  },
+});
+
+const sendMail = async (req) => {
+  const {
+    name, email, subject, message,
+  } = req.body;
+  const info = await transporter.sendMail({
+    from: gmailUser,
+    to: email,
+    subject,
+    text: `${`${name}:  ${message}`}`,
+  });
+  return info.messageId;
+};
+
+export default sendMail;
